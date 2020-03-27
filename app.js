b45b4829b72b3cf592eb7979f34a40be0e0fe4f5
Alan Nguyen <tuan8101@gmail.com>
2020-03-26 21:59:29 -0600

    Create data

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const ItemCtrl = (function() {
     this.name = name;
     this.calories = calories;
   };
+
+  // Data Structure / State
+  const data = {
+    items: [
+      { id: 0, name: 'Steak Dinner', calories: 1200 },
+      { id: 1, name: 'Cookies', calories: 400 },
+      { id: 2, name: 'Yogurt', calories: 200 }
+    ],
+    currentItem: null,
+    totalCalories: 0
+  };
 })();
 
 // UI Controller
