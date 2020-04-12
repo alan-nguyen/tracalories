3919ba11eb893e6229ad7dd1c796256e1ac58d99
Alan Nguyen <tuan8101@gmail.com>
2020-04-12 07:17:35 -0600

    Rename itemEditSubmit to itemEditClick

--- app.js
+++ app.js
@@ -167,7 +167,12 @@ const UICtrl = (function () {
       document.querySelector(UISelectors.backBtn).style.display = 'none';
       document.querySelector(UISelectors.addBtn).style.display = 'inline';
     },
-
+    showEditState: function () {
+      document.querySelector(UISelectors.updateBtn).style.display = 'inline';
+      document.querySelector(UISelectors.deleteBtn).style.display = 'inline';
+      document.querySelector(UISelectors.backBtn).style.display = 'inline';
+      document.querySelector(UISelectors.addBtn).style.display = 'none';
+    },
     getSelectors: function () {
       return UISelectors;
     },
