fe5b538a158a25e4351e9993c62f0a6824eb671d
Alan Nguyen <tuan8101@gmail.com>
2020-04-14 20:41:46 -0600

    Create clearAllItems method

--- app.js
+++ app.js
@@ -84,6 +84,9 @@ const ItemCtrl = (function () {
       // Remove item
       data.items.splice(index, 1);
     },
+    clearAllItems: function () {
+      data.items = [];
+    },
 
     setCurrentItem: function (item) {
       data.currentItem = item;
