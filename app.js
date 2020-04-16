62e91a9d1d0319fb4fcc6778e105c6dcbb7e765f
Alan Nguyen <tuan8101@gmail.com>
2020-04-16 00:07:27 -0600

    Create getItemsFromStorage method

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const StorageCtrl = (function () {
   // Public method
   return {
     storeItem: function (item) {
-      let items = [];
+      let items;
       // Check if any items in LS
       if (localStorage.getItem('items') === null) {
         items = [];
@@ -21,6 +21,15 @@ const StorageCtrl = (function () {
         //Re set LS
         localStorage.setItem('items', JSON.stringify(items));
       }
+    },
+    getItemsFromStorage: function () {
+      let items;
+      if (localStorage.getItem('items') === null) {
+        items = [];
+      } else {
+        items = JSON.parse(localStorage.getItem('items'));
+      }
+      return items;
     },
   };
 })();
